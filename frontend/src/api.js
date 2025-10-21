10f9b8af768528d4c543e463ff9281439a9a8b88
SHAIKAFRIDI-PROG <shaiafridi1610@gmail.com>
2025-10-22 01:03:20 +0530

    Update API_BASE URL to include '/api' suffix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
 // frontend/src/api.js
-const API_BASE = process.env.REACT_APP_API_BASE || "https://expensetrackers-backend.onrender.com";
+const API_BASE = process.env.REACT_APP_API_BASE || "https://expensetrackers-backend.onrender.com/api";
 export default API_BASE;
